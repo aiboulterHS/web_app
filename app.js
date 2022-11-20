c58bd220e5e894fd958105da248bd10bc494af55
aiboulterHS <aiboulter@hubspot.com>
2022-11-20 19:20:45 +0800

    Add paths for page 1,2,3

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const app = express();
 app.get("/", function(req, res){
     res.render('home');
   });
+
+app.get("/page_1", function(req, res){
+  res.render('page_1');
+  });
+
+app.get("/page_2", function(req, res){
+  res.render('page_2');
+  });
+
+app.get("/page_3", function(req, res){
+  res.render('page_3');
+  });
   
 app.listen(3000, function()
   {console.log("Server started on port 3000");}
